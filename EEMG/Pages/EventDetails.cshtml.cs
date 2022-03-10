a15e6416cf350113e3323c82d2bf82961e57c968
Cody Reichard <codyreichard31@gmail.com>
2022-03-09 19:47:39 -0500

    **adding event Pages

---
+++ EEMG/Pages/EventDetails.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EEMG_Login.Data;
+
+namespace EEMG_Login.Pages
+{
+    public class EventDetailsModel : PageModel
+    {
+        private readonly EEMG_Login.Data.ApplicationDbContext _context;
+        public List<Events> Events { get; set; }
+
+        public EventDetailsModel(EEMG_Login.Data.ApplicationDbContext context)
+        {
+            _context = context;
+
+
+            //if(_context.Events.Count > 0) { 
+            //Events events = new Events();
+            //events.EventDate = DateTime.Now;
+            //events.EventTitle = "Test Event";
+            //events.FileName = "test.pptx";
+            //events.FileContents = System.IO.File.ReadAllBytes(@"C:\Users\codyr\OneDrive\Desktop\test.pptx");
+
+            //_context.Events.Add(events);
+
+            //_context.SaveChanges();
+
+            Events = _context.Events.ToList();
+
+        }
+
+
+
+    }
+}
