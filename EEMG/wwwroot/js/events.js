c4f0b4e4da087a2048b839cbd16327c6246bdc95
Cody Reichard <codyreichard31@gmail.com>
2022-03-09 20:19:36 -0500

    ** add events js

---
+++ EEMG/wwwroot/js/events.js
@@ -0,0 +1,9 @@
+﻿$(document).on('click', '#download-ppt', function(event){
+
+    $.get({
+        url: '../../Controllers/EventsController/DownloadPowerPoint',
+        data: { id: -5 }
+    }).done(function () {
+        alert('Added');
+    });
+});
