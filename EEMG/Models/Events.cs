b3d2eb5e17bc23538a91ab97dac75b5266cf61cc
Cody Reichard <codyreichard31@gmail.com>
2022-03-09 19:19:35 -0500

    ** add events model

---
+++ EEMG/Models/Events.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EEMG_Login.Data
+{
+    public class Events
+    {
+        public int Id { get; set; }
+        public DateTime EventDate { get; set; }
+
+        public string EventTitle { get; set; }
+
+        public string FileName { get; set; }
+        public byte[] FileContents { get; set; }
+
+        public Events() { }
+    }
+}
