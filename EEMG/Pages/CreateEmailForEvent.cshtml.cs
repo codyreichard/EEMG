2069edba5fb8c386a8ebeee6169651077bae9dde
Cody Reichard <codyreichard31@gmail.com>
2022-05-29 10:47:47 -0400

    ** updates to the mailing lists ** updates to the menu bar at the top of the website

--- EEMG/Pages/CreateEmailForEvent.cshtml.cs
+++ EEMG/Pages/CreateEmailForEvent.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EEMG.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -14,8 +15,17 @@ namespace EEMG.Pages
         public bool MemberOnly { get; set; }
         public bool EventParticipantsOnly { get; set; }
         public bool EntireMailingList { get; set; }
+        
+        public string LatestEventUrl { get; set; }
 
-        public CreateEmailForEventModel() { }
+        public CreateEmailForEventModel(ApplicationDbContext db) 
+        {
+            var latestEvent = db.Events.OrderBy(e => e.EventDate).FirstOrDefault();
+            if (latestEvent != null)
+            {
+                LatestEventUrl = "https://localhost:44351/Events/EventSignUp/" + latestEvent.Id;
+            }
+        }
         public void OnGet()
         {
         }
--- EEMG/wwwroot/js/site.js
+++ EEMG/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(document).ready(function () {
+    $("ul.navbar-nav > li > a").on("click", function (e) {
+
+        e.preventDefault();
+
+        var link = $(this);
+
+        var listItems = $("ul.navbar-nav > li"); //.removeClass("active");
+
+        //link.parent().addClass("active");
+
+        // Redirect to URL
+        window.location.href = link.attr("href");
+
+    });
+});
